512c22d920cc00cf84e0fd90bc0414911bafef1b
ravinduDon <ravindugunarathna@gmail.com>
2020-05-09 19:35:32 +0530

    authentication complete

--- cyberstonbackendJWTtest/src/main/java/lk/cyberston/v1/cyberston/controllers/TestController.java
+++ cyberstonbackendJWTtest/src/main/java/lk/cyberston/v1/cyberston/controllers/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*",maxAge=3600)
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping("api/test")
 public class TestController {
 
     @GetMapping("/all")
--- cyberstonbackendJWTtest/src/main/java/lk/cyberston/v1/cyberston/security/WebSecurityConfig.java
+++ cyberstonbackendJWTtest/src/main/java/lk/cyberston/v1/cyberston/security/WebSecurityConfig.java
@@ -4,8 +4,11 @@ import lk.cyberston.v1.cyberston.security.jwt.AuthEntryPointJwt;
 import lk.cyberston.v1.cyberston.security.jwt.AuthTokenFilter;
 import lk.cyberston.v1.cyberston.security.servises.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -17,6 +20,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
@@ -36,6 +45,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new AuthTokenFilter();
     }
 
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers(HttpMethod.OPTIONS);
+//    }
+
     @Override
     public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
         authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
@@ -51,12 +65,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer(){
+//        return new WebMvcConfigurerAdapter() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**");
+//            }
+//        };
+//    }
+
+//    @Bean
+//    public FilterRegistrationBean processCorsFilter(){
+//        final UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+//        final CorsConfiguration config=new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.addAllowedOrigin("'");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//        source.registerCorsConfiguration("/**",config);
+//
+//        final FilterRegistrationBean bean=new FilterRegistrationBean(new CorsFilter(source));
+//        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//        return bean;
+//    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/auth.**").permitAll()
+                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
                 .anyRequest().authenticated();
         http.addFilterBefore(authenticationJwtFilter(), UsernamePasswordAuthenticationFilter.class);
