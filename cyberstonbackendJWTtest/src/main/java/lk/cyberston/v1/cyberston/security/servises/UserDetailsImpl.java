7ab48972dcf13bee7677b20c82b5c04aec34b250
ravinduDon <ravindugunarathna@gmail.com>
2020-05-12 11:09:53 +0530

    JWT ok, account ok, error fixed

--- cyberstonbackendJWTtest/src/main/java/lk/cyberston/v1/cyberston/security/servises/UserDetailsImpl.java
+++ cyberstonbackendJWTtest/src/main/java/lk/cyberston/v1/cyberston/security/servises/UserDetailsImpl.java
@@ -50,7 +50,7 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     public Long getId() {
@@ -63,32 +63,32 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 
     @Override
