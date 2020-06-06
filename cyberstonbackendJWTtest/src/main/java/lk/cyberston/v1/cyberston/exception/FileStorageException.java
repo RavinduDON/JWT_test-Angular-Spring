6f8c52b45c9653ba36b018babfb2ce848f7e8f7f
ravinduDon <ravindugunarathna@gmail.com>
2020-06-06 21:35:24 +0530

    db file added

---
+++ cyberstonbackendJWTtest/src/main/java/lk/cyberston/v1/cyberston/exception/FileStorageException.java
@@ -0,0 +1,4 @@
+package lk.cyberston.v1.cyberston.exception;
+
+public class FileStorageException  extends RuntimeException{
+}
---
+++ cyberstonbackendJWTtest/src/main/java/lk/cyberston/v1/cyberston/exception/MyFileNotFoundException.java
@@ -0,0 +1,4 @@
+package lk.cyberston.v1.cyberston.exception;
+
+public class MyFileNotFoundException extends RuntimeException {
+}
